963c7e1d034726a8dd4250deb9e52392c2db8953
nitinajaydixit <nitinajaydixit@gmail.com>
2021-11-27 11:30:59 +0530

    added async and request

---
+++ WebScraping/async.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+/* 
+console.log("Before");
+let data=fs.readFile("f1.txt");
+console.log("After");
+console.log("Mean While"); */
+//async func
+console.log("Before");
+fs.readFile("f1.txt", cb);
+function cb(err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("data: " + data);
+    }
+}
+console.log("After");
+console.log("Mean While");
