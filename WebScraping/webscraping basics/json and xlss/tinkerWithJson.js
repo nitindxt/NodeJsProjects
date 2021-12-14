1fdab91b32783b254e7b3a54df880bb6316c7967
nitinajaydixit <nitinajaydixit@gmail.com>
2021-12-14 11:10:58 +0530

    basic of json and excel

---
+++ WebScraping/webscraping basics/json and xlss/tinkerWithJson.js
@@ -0,0 +1,7 @@
+//usual way to read json
+/* let fs = require('fs');
+let buffer = fs.readFileSync('./example.json');
+console.log(buffer);
+console.log("##################################################");
+let data = JSON.parse(buffer);
+console.log(data); */
